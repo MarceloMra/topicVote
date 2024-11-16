959794feb007f0ac20fc3834d675199a070bc126
MarceloMra <moreira.marcelo321@gmail.com>
2024-11-16 15:20:27 -0300

    TASK-1 - Create initial configuration in the project.

---
+++ src/main/java/com/example/voteTopic/VoteTopicApplication.java
@@ -0,0 +1,13 @@
+package com.example.voteTopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteTopicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/voteTopic/VoteTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voteTopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
